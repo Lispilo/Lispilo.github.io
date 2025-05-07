23aa567b350e66f635df0843ded91cae9df8b39e
Lispilo <96479509+Lispilo@users.noreply.github.com>
2025-05-07 14:46:02 +0000

    adding animation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 import { animate } from 'animejs';
+
+animate('span', {
+    y: [
+        { to: '-2.75rem', ease: 'outExpo', duration: 600 },
+        { to: 0, ease: 'outBounce', duration: 800, delay: 100 }
+    ],
+    rotate: {
+        from: '-1turn',
+        delay: 0
+    },
+    delay: (_, i) => i * 50,
+    ease: 'inOutCirc',
+    loopDelay: 1000,
+    loop: false
+});
